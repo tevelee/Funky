f27e3a7d0e09ca8bd28fa790d1644a1815715db8
László Teveli <laszlo.teveli@skyscanner.net>
2017-04-08 15:51:38 +0200

    Added tests for dictionary subclasses

--- Example/Tests/Interfaces/Dictionary/FunkyNSDictionaryPrefixedUtilitiesTests.m
+++ Example/Tests/Interfaces/Dictionary/FunkyNSDictionaryPrefixedUtilitiesTests.m
@@ -8,7 +8,7 @@
 
 #import <XCTest/XCTest.h>
 #import <Funky/FunkyDictionaryUtilities.h>
-#import <Funky/NSDictionary+FunkyUtilities.h>
+#import <Funky/NSDictionary+FunkyPrefixedUtilities.h>
 #import <Funky/NSDictionary+FunkyCore.h>
 #import <OCMock/OCMock.h>
 #import "MockedDictionaryUtilities.h"
@@ -35,7 +35,7 @@
 
 - (void)test_all_callsUtilitiesClass
 {
-    [dictionary all:^BOOL(id key, id item) {
+    [dictionary funky_all:^BOOL(id key, id item) {
         return item != nil;
     }];
     OCMVerify([mockedDictionaryUtils all:[OCMArg any]]);
@@ -43,7 +43,7 @@
 
 - (void)test_none_callsUtilitiesClass
 {
-    [dictionary none:^BOOL(id key, id item) {
+    [dictionary funky_none:^BOOL(id key, id item) {
         return item != nil;
     }];
     OCMVerify([mockedDictionaryUtils none:[OCMArg any]]);
@@ -51,7 +51,7 @@
 
 - (void)test_contains_callsUtilitiesClass
 {
-    [dictionary contains:^BOOL(id key, id item) {
+    [dictionary funky_contains:^BOOL(id key, id item) {
         return item != nil;
     }];
     OCMVerify([mockedDictionaryUtils contains:[OCMArg any]]);
@@ -59,7 +59,7 @@
 
 - (void)test_count_callsUtilitiesClass
 {
-    [dictionary count:^BOOL(id key, id item) {
+    [dictionary funky_count:^BOOL(id key, id item) {
         return item != nil;
     }];
     OCMVerify([mockedDictionaryUtils count:[OCMArg any]]);
@@ -67,7 +67,7 @@
 
 - (void)test_map_callsUtilitiesClass
 {
-    [dictionary map:^FunkyPair*(id key, id item) {
+    [dictionary funky_map:^FunkyPair*(id key, id item) {
         return [FunkyPair pairWithKey:key value:item];
     }];
     OCMVerify([mockedDictionaryUtils map:[OCMArg any]]);
@@ -75,7 +75,7 @@
 
 - (void)test_filter_callsUtilitiesClass
 {
-    [dictionary filter:^BOOL(id key, id item) {
+    [dictionary funky_filter:^BOOL(id key, id item) {
         return item != nil;
     }];
     OCMVerify([mockedDictionaryUtils filter:[OCMArg any]]);
@@ -83,7 +83,7 @@
 
 - (void)test_reduce_callsUtilitiesClass
 {
-    [dictionary reduce:^id(id previousValue, id key, id item) {
+    [dictionary funky_reduce:^id(id previousValue, id key, id item) {
         return 0;
     } withInitialValue:@0];
     OCMVerify([mockedDictionaryUtils reduce:[OCMArg any] withInitialValue:[OCMArg any]]);
@@ -91,13 +91,13 @@
 
 - (void)test_merged_callsUtilitiesClass
 {
-    [dictionary merged:@{@5: @"5", @6: @"6"}];
+    [dictionary funky_merged:@{@5: @"5", @6: @"6"}];
     OCMVerify([mockedDictionaryUtils merged:[OCMArg any]]);
 }
 
 - (void)test_forEach_callsUtilitiesClass
 {
-    [dictionary forEach:^(id key, id item) {
+    [dictionary funky_forEach:^(id key, id item) {
     }];
     OCMVerify([mockedDictionaryUtils forEach:[OCMArg any]]);
 }
